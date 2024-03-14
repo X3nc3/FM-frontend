63c6c241308c2220ac0ee5e2ca5d9415d72e6824
Maxence <maxence.ogier@gmail.com>
2024-03-14 12:38:11 +0100

    create structure

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <header>
+      <div>Home</div>
+    </header>
+  );
+};
+
+export default Home;
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <header>
+      <div>Nav</div>
+    </header>
+  );
+};
+
+export default Nav;
